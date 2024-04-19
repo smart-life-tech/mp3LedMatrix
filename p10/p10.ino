eb34ee25a99d1f93c24005169591bfe7abe6051c
smart-life-tech <christlightworld@gmail.com>
2024-04-19 21:03:34 +0100

    modified:   p10/p10.ino

--- p10/p10.ino
+++ p10/p10.ino
@@ -203,7 +203,7 @@ void loop()
             buzzerFunc(HIGH);
             int bar = map(score, 0, 999, 0, NUM_LEDS);
             colorWipe(CRGB::Red, 60, bar);
-            sprintf(message, "  %d   ", score);
+            sprintf(message, "%d   ", score);
             // dmd.clearScreen(true);
             dmd.drawString(3, 18, maxMessage, 9, GRAPHICS_NORMAL);
             dmd.drawString(3, 0, message, 10, GRAPHICS_NORMAL);
